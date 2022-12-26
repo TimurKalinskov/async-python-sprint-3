32c40329cc967240ea18a13f99c361bd284821b8
Sergey Gavrilenko <gryroach@gmail.com>
2022-12-26 23:41:06 +0300

    1. created server and client in console

--- client.py
+++ client.py
@@ -1,10 +1,52 @@
-# import asyncio
+import asyncio
 # import aiohttp
+from asyncio.streams import StreamReader, StreamWriter
+from threading import Thread
 
 
 class Client:
-    def __init__(self, server_host="127.0.0.1", server_port=8000):
-        pass
+    def __init__(self, nick: str, server_host='127.0.0.1', server_port=8000):
+        self.nick = nick
+        self.server_host = server_host
+        self.server_port = server_port
+        self.writer = None
+        self.reader = None
+        self.event_loop = asyncio.new_event_loop()
 
-    def send(self, message=""):
-        pass
+    def connect(self):
+        Thread(
+            target=self._loop_in_thread, args=(self.event_loop,), daemon=True
+        ).start()
+
+    def send_message(self, message: str = ''):
+        asyncio.run_coroutine_threadsafe(self.send(message), self.event_loop)
+
+    def _loop_in_thread(self, loop):
+        asyncio.set_event_loop(loop)
+        asyncio.get_event_loop().create_task(self.main())
+        loop.run_forever()
+
+    async def main(self):
+        self.reader, self.writer = await asyncio.open_connection(
+            self.server_host, self.server_port
+        )
+        while True:
+            data = await self.reader.read(1024)
+            print(data.decode())
+            if not data:
+                break
+        print('Close the connection')
+        self.writer.close()
+
+    async def send(self, message: str = ''):
+        self.writer.write(f'{self.nick} * {message}'.encode())
+        await self.writer.drain()
+
+
+if __name__ == '__main__':
+    client = Client('test')
+    client.connect()
+    # client.send_message('Hello')
+
+
+
--- server.py
+++ server.py
@@ -1,9 +1,50 @@
-# import asyncio
+import logging
+import sys
+import asyncio
+from asyncio.streams import StreamReader, StreamWriter
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
 
 class Server:
     def __init__(self, host="127.0.0.1", port=8000):
-        pass
+        self.host: str = host
+        self.port: int = port
+        self.writers: list[StreamWriter] = []
 
     def listen(self):
-        pass
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.main())
+
+    async def main(self):
+        srv = await asyncio.start_server(
+            self.client_connected, self.host, self.port)
+        async with srv:
+            await srv.serve_forever()
+
+    async def client_connected(
+            self, reader: StreamReader, writer: StreamWriter):
+        address = writer.get_extra_info('peername')
+        logger.info('Start serving %s', address)
+        self.writers.append(writer)
+        await self.send_message(writer, address, 'New guest in chat!')
+
+        while True:
+            data = await reader.read(1024)
+            if not data:
+                break
+            await self.send_message(writer, address, data)
+
+        logger.info('Stop serving %s', address)
+        writer.close()
+
+    async def send_message(self, self_writer, addr, message):
+        if isinstance(message, bytes):
+            message = message.decode()
+        for w in self.writers:
+            if w != self_writer:
+                w.write(f"{addr}: {message}\n".encode())
+                await w.drain()
